af078718af6e02f9d252c379b9d5a863e8467471
cstenkamp <cstenkamp@uos.de>
2017-07-10 14:54:18 +0200

    know why its different...

--- memorytest2DELETEME.py
+++ memorytest2DELETEME.py
@@ -28,17 +28,39 @@ MEMSIZE = 350
 
 m1 =   Precisememory(MEMSIZE, containers)
 
-m2 = Efficientmemory(MEMSIZE, containers, 4) 
+for i in range(2,300):
+    print(i, np.all(m1[i][0][0][0] == m1[i+1][0][0][1]), m1[i][4])  
+    #wir sehen, das gilt immer, bis auf beim Reset. Warum da nicht? weil er da den inputval RESETTET -> setzt auf nur nullen
+
+print("")
+print("")   
+
+for i in range(2,300):
+    if m1[i-1][4]:
+        print(i, np.count_nonzero(m1[i][0][0][1]))
+        print(i, np.count_nonzero(m1[i][0][0][2]))
+        print(i, np.count_nonzero(m1[i][0][0][3]))
+        
+#es gilt also: WENN er IM FRAME VORHER resettet hat, DANN steht in der history nur zeros
+#allerdings, und das ist mein fehler, steht DAS NEUESTE FRAME ZUERST        
 
 
-print(m2._pointer) #immer der in pointer bis pointer+3 ist falsch
-for i in range(MEMSIZE):
-    if m2[i] != False and m2[i] is not None:
-        print(i, np.all(m1[i][0][0][0] == m2[i][0][0][0]))
-    elif m2[i] == False:
-        print(i, "corrupted")
-    else:
-        print(i, "empty")
+#print(m1[298][0][0][0])
+#print(m1[299][0][0][1])
+
+
+
+#m2 = Efficientmemory(MEMSIZE, containers, 4) 
+#
+#
+#print(m2._pointer) #immer der in pointer bis pointer+3 ist falsch
+#for i in range(MEMSIZE):
+#    if m2[i] != False and m2[i] is not None:
+#        print(i, np.all(m1[i][0][0][0] == m2[i][0][0][0]))
+#    elif m2[i] == False:
+#        print(i, "corrupted")
+#    else:
+#        print(i, "empty")
 #
 #print("")
 #print("")
