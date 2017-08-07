544c58072ad0648fa3560f89a17ef83036e2bede
cstenkamp <cstenkamp@uos.de>
2017-08-07 17:54:24 +0200

    I give up

--- ddpg.py
+++ ddpg.py
@@ -13,16 +13,23 @@ import math
 import time
 from utils import convolutional_layer, fc_layer, variable_summary
 import tflearn
+import tensorflow.contrib.slim as slim
 
 
 #bases heavily on http://pemami4911.github.io/blog/2016/08/21/ddpg-rl.html
 
 
+def dense(x, units, activation=tf.identity, decay=None, minmax=None):
+    if minmax is None:
+        minmax = float(x.shape[1].value) ** -.5
+    return tf.layers.dense(x, units,activation=activation, kernel_initializer=tf.random_uniform_initializer(-minmax, minmax), kernel_regularizer=decay and tf.contrib.layers.l2_regularizer(1e-3))
+
+    
 class criticNet() :
     
-    #TODO: ff_inputs, global_step
-    def __init__(self, conf, agent, outerscope="critic", name="online"):
-        with tf.variable_scope(name, reuse=None):
+    #TODO: ff_inputs, global_step, weight decay!
+    def __init__(self, conf, agent, outerscope="critic", name="online", reuse=False):
+        with tf.variable_scope(name, reuse=reuse):
             self.conf = conf
             self.agent = agent
             self.name = name  
@@ -31,32 +38,33 @@ class criticNet() :
             self.conv_stacksize = (self.conf.history_frame_nr*2 if self.conf.use_second_camera else self.conf.history_frame_nr) if self.agent.conv_stacked else 1
             
             self.conv_inputs = tf.placeholder(tf.float32, shape=[None, self.conf.image_dims[0], self.conf.image_dims[1], self.conv_stacksize], name="conv_inputs")  
-            self.actions = tf.placeholder(tf.float32, shape=[None, 3], name="action_inputs")  
+            self.actions = tf.placeholder(tf.float32, shape=[None, self.conf.num_actions], name="action_inputs")  
             
             rs_input = tf.reshape(self.conv_inputs, [-1, self.conf.image_dims[0], self.conf.image_dims[1], self.conv_stacksize]) #final dimension = number of color channels*number of stacked (history-)frames                  
-            #convolutional_layer(input_tensor, input_channels, kernel_size, stride, output_channels, name, act, is_trainable, batchnorm, is_training, weightdecay=False, pool=True, trainvars=None, varSum=None, initializer=None)
-            conv1 = convolutional_layer(rs_input, self.conv_stacksize, [4,6], [2,3], 32, "Conv1", tf.nn.relu, True, True, True, False, False, {}, variable_summary, initializer="fanin") #(?, 14, 14, 32)
-            conv2 = convolutional_layer(conv1, 32, [4,4], [2,2], 64, "Conv2", tf.nn.relu, True, True, True, False, False, {}, variable_summary, initializer="fanin")                     #(?, 8, 8, 64)
-            conv3 = convolutional_layer(conv2, 64, [3,3], [2,2], 64, "Conv3", tf.nn.relu, True, True, True, False, True, {}, variable_summary, initializer="fanin")                      #(?, 2, 2, 64)
-            conv4 = convolutional_layer(conv3, 64, [4,4], [2,2], self.h_size, "Conv4", tf.nn.relu, True, True, True, False, False, {}, variable_summary, initializer="fanin")            #(?, 1, 1, 256)
-            conv4_flat = tf.reshape(conv4, [-1, self.h_size])
-            fc1 = fc_layer(conv4_flat, self.h_size, 100, "FC1", True, True, True, False, tf.nn.relu, 1, {}, variable_summary, initializer="fanin")                 
-            fc1 = tf.concat([fc1, self.actions], 1) 
-            fc2 = fc_layer(fc1, 103, 50, "FC2", True, True, True, False, tf.nn.relu, 1, {}, variable_summary, initializer="fanin")                 
-            self.Q = fc_layer(fc2, 50, 1, "FC3", True, False, True, False, tf.identity, 1, {}, variable_summary, initializer=tf.random_uniform_initializer(-3e-4, 3e-4))                 
-        
-            self.ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope=outerscope+"/"+self.name)      
-            self.trainables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=outerscope+"/"+self.name)
+            
+            self.conv1 = slim.conv2d(inputs=rs_input,num_outputs=32,kernel_size=[4,6],stride=[2,3],padding='VALID', biases_initializer=None, normalizer_fn=slim.batch_norm)
+            self.conv2 = slim.conv2d(inputs=self.conv1,num_outputs=64,kernel_size=[4,4],stride=[2,2],padding='VALID', biases_initializer=None, normalizer_fn=slim.batch_norm)
+            self.conv3 = slim.conv2d(inputs=self.conv2,num_outputs=64,kernel_size=[3,3],stride=[1,1],padding='VALID', biases_initializer=None, normalizer_fn=slim.batch_norm)
+            self.conv4 = slim.conv2d(inputs=self.conv3,num_outputs=self.h_size, kernel_size=[4,4],stride=[1,1],padding='VALID', biases_initializer=None, normalizer_fn=slim.batch_norm)          
+            self.conv4_flat = tf.reshape(self.conv4, [-1, self.h_size])
+            
+            fc1 = tf.concat([self.conv4_flat, self.actions], 1) 
+            fc2 = dense(fc1, 50, tf.nn.relu, decay=None)
+            self.Q = dense(fc2, 1, decay=True, minmax=1e-4)
+            
+        self.ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope=outerscope+"/"+self.name)      
+        self.losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES, scope=outerscope+"/"+self.name)      
+        self.trainables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=outerscope+"/"+self.name)
         
 
 
 
 class actorNet():
-    def __init__(self, conf, agent, bounds, outerscope="actor", name="online"):
-        tanh_min_bounds,tanh_max_bounds = np.array([-1, -1, -1]), np.array([1, 1, 1])
-        min_bounds, max_bounds = np.array(list(zip(*bounds)))
+    def __init__(self, conf, agent, bounds, outerscope="actor", name="online", reuse=False):
+#        tanh_min_bounds,tanh_max_bounds = np.array([-1, -1, -1]), np.array([1, 1, 1])
+#        min_bounds, max_bounds = np.array(list(zip(*bounds)))
         
-        with tf.variable_scope(name, reuse=None):
+        with tf.variable_scope(name, reuse=reuse):
             self.conf = conf
             self.agent = agent
             self.name = name  
@@ -69,17 +77,21 @@ class actorNet():
 
             rs_input = tf.reshape(self.conv_inputs, [-1, self.conf.image_dims[0], self.conf.image_dims[1], self.conv_stacksize]) #final dimension = number of color channels*number of stacked (history-)frames                  
             #convolutional_layer(input_tensor, input_channels, kernel_size, stride, output_channels, name, act, is_trainable, batchnorm, is_training, weightdecay=False, pool=True, trainvars=None, varSum=None, initializer=None)
-            conv1 = convolutional_layer(rs_input, self.conv_stacksize, [4,6], [2,3], 32, "Conv1", tf.nn.relu, True, True, True, False, False, {}, variable_summary, initializer="fanin") #(?, 14, 14, 32)
-            conv2 = convolutional_layer(conv1, 32, [4,4], [2,2], 64, "Conv2", tf.nn.relu, True, True, True, False, False, {}, variable_summary, initializer="fanin")                     #(?, 8, 8, 64)
-            conv3 = convolutional_layer(conv2, 64, [3,3], [2,2], 64, "Conv3", tf.nn.relu, True, True, True, False, True, {}, variable_summary, initializer="fanin")                      #(?, 2, 2, 64)
-            conv4 = convolutional_layer(conv3, 64, [4,4], [2,2], self.h_size, "Conv4", tf.nn.relu, True, True, True, False, False, {}, variable_summary, initializer="fanin")            #(?, 1, 1, 256)
-            conv4_flat = tf.reshape(conv4, [-1, self.h_size])
-            fc1 = fc_layer(conv4_flat, self.h_size, 100, "FC1", True, True, True, False, tf.nn.relu, 1, {}, variable_summary, initializer="fanin")                 
-            self.fc2 = fc_layer(fc1, 100, 3, "FC2", True, False, True, False, tf.nn.tanh, 1, {}, variable_summary, initializer="fanin")                  #kein batchnorm im letzten layer sonst ist tanh größer 1!
-            self.scaled = (((self.fc2 - tanh_min_bounds)/ (tanh_max_bounds - tanh_min_bounds)) * (max_bounds - min_bounds) + min_bounds) #broadcasts the bound arrays
-
+           
+            self.conv1 = slim.conv2d(inputs=rs_input,num_outputs=32,kernel_size=[4,6],stride=[2,3],padding='VALID', biases_initializer=None, normalizer_fn=slim.batch_norm)
+            self.conv2 = slim.conv2d(inputs=self.conv1,num_outputs=64,kernel_size=[4,4],stride=[2,2],padding='VALID', biases_initializer=None, normalizer_fn=slim.batch_norm)
+            self.conv3 = slim.conv2d(inputs=self.conv2,num_outputs=64,kernel_size=[3,3],stride=[1,1],padding='VALID', biases_initializer=None, normalizer_fn=slim.batch_norm)
+            self.conv4 = slim.conv2d(inputs=self.conv3,num_outputs=self.h_size, kernel_size=[4,4],stride=[1,1],padding='VALID', biases_initializer=None, normalizer_fn=slim.batch_norm)          
+            self.conv4_flat = tf.reshape(self.conv4, [-1, self.h_size])
+            
+            fc1 = dense(self.conv4_flat, 50, tf.nn.relu)
+            self.fc2 = dense(fc1, self.num_actions, tf.nn.tanh, minmax = 3e-4)
+            self.outs = self.fc2
+#            self.scaled = (((self.outs - tanh_min_bounds)/ (tanh_max_bounds - tanh_min_bounds)) * (max_bounds - min_bounds) + min_bounds) #broadcasts the bound arrays
+            self.scaled = self.outs
 
         self.ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope=outerscope+"/"+self.name)      
+        self.losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES, scope=outerscope+"/"+self.name)      
         self.trainables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=outerscope+"/"+self.name)
 
 ##########################################################################################          
@@ -114,7 +126,7 @@ class actor():
             self.conf = conf
             self.agent = agent
             self.session = session
-            self.learning_rate = 0.0001 #0.0001
+            self.learning_rate = 0.001 #0.0001
             bounds = [(0, 1), (0, 1), (-1, 1)]
             
             self.online = actorNet(conf, agent, bounds)
@@ -126,9 +138,10 @@ class actor():
             self.action_gradient = tf.placeholder(tf.float32, [None, self.conf.num_actions], name="actiongradient")        
             
             # Combine the gradients here
-            self.actor_gradients = tf.gradients(self.online.scaled, self.online.trainables, -self.action_gradient)  #we want gradient AScent
+            self.actor_gradients = tf.gradients(self.online.outs, self.online.trainables, -self.action_gradient)  #we want gradient AScent
             
-            self.optimize = tf.train.AdamOptimizer(self.learning_rate).apply_gradients(zip(self.actor_gradients, self.online.trainables))
+            with tf.control_dependencies(self.online.ops):
+                self.optimize = tf.train.AdamOptimizer(self.learning_rate).apply_gradients(zip(self.actor_gradients, self.online.trainables))
 
         
     def make_inputs(self, inputs):
@@ -136,7 +149,7 @@ class actor():
         return conv_inputs
                 
     def train(self, inputs, actiongrad):
-        self.session.run(self.optimize, feed_dict={self.online.conv_inputs: self.make_inputs(inputs), self.action_gradient: actiongrad})
+        return self.session.run([self.online.outs, self.optimize], feed_dict={self.online.conv_inputs: self.make_inputs(inputs), self.action_gradient: actiongrad})
         
     def predict(self, inputs, which="target"):
         net = self.online if which == "online" else self.target
@@ -160,22 +173,28 @@ class critic():
             
             self.smoothTargetUpdate = _netCopyOps(self.online, self.target, self.conf.target_update_tau)      
                 
-            # Network target (y_i)
-            self.predicted_q_value = tf.placeholder(tf.float32, [None, 1], name="predQVal")        
+            self.target_Q = tf.placeholder(tf.float32, [None, 1], name="target_Q")        
                 
-            self.loss = tf.reduce_mean(tf.square(self.predicted_q_value - self.online.Q))
-            self.optimize = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)    
+#            self.loss = tf.reduce_mean(tf.square(self.target_Q - self.online.Q))
+            self.loss = tf.losses.mean_squared_error(self.target_Q, self.online.Q)
+            
+            with tf.control_dependencies(self.online.ops):
+                self.optimize = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)    
             
             self.action_grads = tf.gradients(self.online.Q, self.online.actions) #letztere sind ja placeholders, kommend vom actor
-            self.action_grads = tf.stop_gradient(self.action_grads)
+#            self.action_grads = tf.stop_gradient(self.action_grads)
 
     def make_inputs(self, inputs):
         conv_inputs = [inputs[i][0] for i in range(len(inputs))]
         return conv_inputs        
     
-    def train(self, inputs, action, predQVal):
-        return self.session.run([self.online.Q, self.optimize], feed_dict={self.online.conv_inputs: self.make_inputs(inputs), \
-                                                                      self.online.actions: action, self.predicted_q_value: predQVal})        
+    def train(self, inputs, actions, predQVals):
+        return self.session.run([self.online.Q, self.loss, self.optimize], feed_dict={self.online.conv_inputs: self.make_inputs(inputs), \
+                                                                      self.online.actions: actions, self.target_Q: predQVals})      
+        
+    def TDError(self, inputs, actions, predQVals):
+        return self.session.run([self.loss], feed_dict={self.online.conv_inputs: self.make_inputs(inputs), \
+                                                                      self.online.actions: actions, self.target_Q: predQVals})           
         
     def predict(self, inputs, action, which="target"):
         net = self.online if which == "online" else self.target
@@ -197,59 +216,71 @@ class DDPG_model():
         self.conf = conf
         self.agent = agent
         self.session = session
+        self.initNet()
         
-        self.actor = actor(conf, agent, session)
-        self.critic = critic(conf, agent, session)
+        
+    def initNet(self):
+        self.actor = actor(self.conf, self.agent, self.session)
+        self.critic = critic(self.conf, self.agent, self.session)
         
         self.session.run(tf.global_variables_initializer())
         _runMultipleOps(_netCopyOps(self.actor.target, self.actor.online), self.session)
         _runMultipleOps(_netCopyOps(self.critic.target, self.critic.online), self.session)
         
         
-    #actor predicts action. critic predicts q-value of action. That is compared to the *actual* q-value of action.
-    #critic gets trained with the actual q-values. online-actor predicts new actions. actor uses critic's gradients to train, too.
+    #actor predicts action. critic predicts q-value of action. That is compared to the actual q-value of action. (TD-Error)
+    #online-actor predicts new actions. actor uses critic's gradients to train, too.
     def train_step(self, batch):
         oldstates, actions, rewards, newstates, terminals = batch
-        target_q = self.critic.predict(newstates, self.actor.predict(newstates))
-        cumrewards = [rewards[i] if terminals[i] else rewards[i]+self.conf.q_decay*target_q[i] for i in range(len(rewards))]
-        predicted_q_value, _ =  self.critic.train(oldstates, actions, cumrewards)
+        #Training the critic...
+        target_Q = self.critic.predict(newstates, self.actor.predict(newstates))
+        cumrewards = [rewards[i] if terminals[i] else rewards[i]+self.conf.q_decay*target_Q[i] for i in range(len(rewards))]
+        predicted_q_value, TDError, _ =  self.critic.train(oldstates, actions, cumrewards)
+        #Training the actor...
         a_outs = self.actor.predict(oldstates, "online")
         grads = self.critic.action_gradients(oldstates, a_outs)
-        self.actor.train(oldstates, grads[0])
+        actorOuts, _ = self.actor.train(oldstates, grads[0])
+#        print(np.mean(np.array([abs(actorOuts[i][0] -actions[i][0]) for i in range(len(actions))])))
         
-        self.actor.update_target()
+        
         self.critic.update_target()
+        self.actor.update_target()
+        
         
     def inference(self, statesBatch):
         return self.actor.predict(statesBatch)
 
-    def old_evaluate(self, batch):
+        
+    def evaluate(self, batch):
         oldstates, actions, rewards, newstates, terminals = batch
-        result = np.array([np.argmax(self.agent.discretize(*i)) for i in self.actor.predict(oldstates)])
-        human = np.array([np.argmax(myAgent.discretize(*i)) for i in actions])
-        return np.mean(np.array(human == result, dtype=int))
+#        result = np.array([np.argmax(self.agent.discretize(*i)) for i in self.actor.predict(oldstates)])
+#        human = np.array([np.argmax(myAgent.discretize(*i)) for i in actions])
+        actorOuts = self.actor.predict(oldstates)
+        return np.mean(np.array([abs(actorOuts[i][0] -actions[i][0]) for i in range(len(actions))]))
+        
         
     def eval_TDError(self, batch):
         oldstates, actions, rewards, newstates, terminals = batch
-        target_q = self.critic.predict(newstates, self.actor.predict(newstates))
-        cumrewards = [rewards[i] if terminals[i] else rewards[i]+self.conf.q_decay*target_q[i] for i in range(len(rewards))]
-        predicted_q_value =  self.critic.predict(oldstates, actions)
-        return(np.mean(abs(np.array(cumrewards)-predicted_q_value)))
+        target_Q = self.critic.predict(newstates, self.actor.predict(newstates))
+        cumrewards = [rewards[i] if terminals[i] else rewards[i]+self.conf.q_decay*target_Q[i] for i in range(len(rewards))]
+        loss =  self.critic.TDError(oldstates, actions, cumrewards)
+        return loss[0]
         
         
 ##########################################################################################
 
 
 def TPSample(conf, agent, batchsize):
-    return read_supervised.create_QLearnInputs_from_PTStateBatch(*trackingpoints.next_batch(conf, agent, batchsize), agent)
-#    tmp[1] = [i[2] for i in tmp[1]]
-#    return tmp 
+    tmp = list(read_supervised.create_QLearnInputs_from_PTStateBatch(*trackingpoints.next_batch(conf, agent, batchsize), agent))
+    tmp[1] = [[i[2]] for i in tmp[1]]
+    return tmp 
     
 
 if __name__ == '__main__':       
     import config
     conf = config.Config()
-    conf.target_update_tau = 1
+    conf.target_update_tau = 1e-3
+    conf.num_actions = 1
     import read_supervised
     from server import Containers; 
     import dqn_rl_agent
@@ -263,14 +294,17 @@ if __name__ == '__main__':
     
     #TODO: einen continous agent machen, bei dem getnetusableaction nicht argmax returned!
     
+#    trainBatch = TPSample(conf, myAgent, trackingpoints.numsamples)
+#    print(trainBatch[2][1])
+    
     for i in range(10000):
         trackingpoints.reset_batch()
         trainBatch = TPSample(conf, myAgent, trackingpoints.numsamples)
-        print("Iteration", i, "Accuracy",model.old_evaluate(trainBatch))  
-        print(model.eval_TDError(trainBatch))
+        print("Iteration", i, "Accuracy (0 is best)",model.evaluate(trainBatch))  
+        print("TD-Error vom Critic:",model.eval_TDError(trainBatch))
         if i % 10 == 0:
-            print(model.inference(trainBatch[0][:2])) #die ersten 2 states   
-            print(trainBatch[1][:2])
+            print(np.array(model.inference(trainBatch[0][:2]))) #die ersten 2 states   
+            print(np.array(trainBatch[1][:2]))
         trackingpoints.reset_batch()     
         while trackingpoints.has_next(BATCHSIZE):
             trainBatch = TPSample(conf, myAgent, BATCHSIZE)
