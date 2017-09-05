ac21b4cd3e3e08eb3110af6c7f11beaba2547cac
cstenkamp <cstenkamp@uos.de>
2017-09-05 12:15:34 +0200

    letstry

--- agent.py
+++ agent.py
@@ -98,7 +98,7 @@ class AbstractAgent(object):
         if self.use_evaluator:
             self.evaluator = evaluator(self.containers, self, self.show_plots, self.conf.save_xml,      \
                                        ["average rewards", "average Q-vals", "progress", "laptime"                    ], \
-                                       [(-0.1,1.3),        (-1,100),          100,        self.conf.time_ends_episode ] ) 
+                                       [(-0.1,1.3),        (-1,100),          100,        self.time_ends_episode ] ) 
 
         #statecounterstuff deleteme
 #        self.allN = len(self.memory)
